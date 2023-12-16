b21cf82eb82fc383f5afd3ecc4c4fb46d722ad24
JayAlvinCVallescas <202080209@psu.palawan.edu.ph>
2023-12-16 22:18:56 +0800

    transfer files

---
+++ hardwaretroubleshoot/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ComputerTroubleshoot from './ComputerTroubleshoot.jsx'
+// import './ComputerTroubleshoot.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ComputerTroubleshoot />
+  </React.StrictMode>,
+)
